d4e45b6b7b7d08a81e037b7f111740b5387a127a
jinjuwon <drasgon@naver.com>
2022-11-18 02:15:02 +0900

    'Docker monitoring tool complete'

--- src/main/java/Monitoring.java
+++ src/main/java/Monitoring.java
@@ -33,10 +33,10 @@ class RxTx
 
     @Override
     public String toString() {
-        return "rx byte : "+this.RxByte+'\n' +
-                "rx packet : "+this.RxPacket+'\n'+
-                "tx byte : "+this.TxByte+'\n'+
-                "tx packet : "+this.TxPacket+"\n";
+        return " rx byte : "+this.RxByte+
+                " rx packet : "+this.RxPacket+
+                " tx byte : "+this.TxByte+
+                " tx packet : "+this.TxPacket;
     }
 }
 
@@ -44,23 +44,26 @@ public class Monitoring {
     static RxTx[] packet_arr = new RxTx[1000]; // 충분히 큰 길이의 배열을 선언하여 각 컨테이너에 대한 네트워크 패킷 정보 유지
     static Long restMemory = 0l;
     static Long totalUseMemory = 0l;
+    static List<String> logs = new ArrayList<>();
 
     public static void main(String[] args) throws IOException, InterruptedException
     {
-        String path = "/Users/jinjuwon/Documents/mac_git/demo/dockerhw/src/main/java/result.txt";
-        JSONObject json = new JSONObject();
+        // mac 형식의 파일 경로입니다. 사용자의 컴퓨터에 따라 변경하여 사용
+        String path = "/Users/jinjuwon/Documents/mac_git/demo/dockerhw/src/main/java/result2.txt";
+
         ObjectMapper mapper = new ObjectMapper(); // json에 대한 정보를 별도의 클래스로 유지하는 방법도 있지만 자바의 ObjectMapper를 통해 클래스를 생성하지 않고도 확인 가능
         OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class); // 자바에서 기본 OS 정보 확인을 위해서는 osBean 객체를 활용해야 한다
-        PrintWriter out = new PrintWriter(new FileWriter(path,true),true);
         while(true)
         {
             // Docker Engine 사용을 위한 코드
             Process process = Runtime.getRuntime().exec("curl -s --unix-socket /var/run/docker.sock http://v1.41/containers/json");
             BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
-            String str;
+            JSONObject json = new JSONObject();
+
             while((line=in.readLine())!=null)
             {
+                PrintWriter out = new PrintWriter(new FileWriter(path,true));
                 List<Map<String,Object>> datas = mapper.readValue(line, new TypeReference<>() {}); // 정보에는 현재 동작중인 컨테이너의 json 정보가 json array 형태
                 System.out.println("container num : "+ datas.size()); // List size가 곧 컨테이너의 개수
                 json.put("container num",datas.size());
@@ -74,26 +77,28 @@ public class Monitoring {
                 System.out.println("호스트 메모리 크기 총량 : "+ memoryString + "MB");
                 json.put("host memory(MB)",memoryString);
 
+
+                Thread.sleep(500);
                 // 컨테이너에 대한 각 정보를 출력하고 총 cpu 사용량을 반환하여 유휴 cpu %를 계산
                 Double restCpu = availableCore - print(datas,json);
+                Thread.sleep(500);
 
-                // host 유휴 메모리 확인
-                long host_rest_memory = (osBean.getFreePhysicalMemorySize() + osBean.getFreeSwapSpaceSize()) /1024/1024;
+                // host 유휴 메모리 확인 JVM내의 메모리가 아닌 OS 메모리
+                long host_rest_memory = (osBean.getFreePhysicalMemorySize() + osBean.getFreeSwapSpaceSize())/1024/1024;
                 String restMemoryString = host_rest_memory+"";
                 System.out.println("호스트 유휴 메모리 : "+restMemoryString+"MB");
                 json.put("host 유휴 memory",restMemoryString);
                 System.out.println("호스트 유휴 CPU : "+restCpu+"%");
                 json.put("host 유휴 CPU", restCpu);
 
-
                 // json 로그 형태로 파일에 저장
-                try (out) {
-                    out.write(json.toString());
-                    out.write("\n");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                System.out.println(json.toJSONString());
+                out.write(json.toJSONString());
+                out.write("\n");
+                out.flush();
+                out.close();
             }
+
             System.out.println("---------------------------Monitor Sleep-------------------------------------------------------------------");
             Thread.sleep(2000); // 모니터링 주기 2초
             in.close();
@@ -104,28 +109,25 @@ public class Monitoring {
     {
         int length = datas.size(); // container 개수
         double CpuUse =0f; // 모든 컨테이너의 cpu 사용량을 더하여 총 cpu 사용량을 구한다
-
         int i=0;
         totalUseMemory=0l;
         for(Map<String,Object> data : datas) // 컨테이너 개수만큼 반복문 실행
         {
+            JSONObject object = new JSONObject();
 
             Map<String,Object> first = (Map<String,Object>)data.get("NetworkSettings");
             Map<String,Object> second = (Map<String,Object>)first.get("Networks");
             Map<String,Object> IpAddress = (Map<String,Object>)second.get("bridge");
             System.out.println("----------------Container------------------");
             System.out.println("IpAddress : "+IpAddress.get("IPAddress")); // 컨테이너의 IP 주소
-            json.put("IPAddress"+i ,IpAddress.get("IPAddress"));
-            Thread.sleep(100);
+            object.put("IP Address",IpAddress.get("IPAddress"));
             System.out.println("Process Id : "+data.get("Id")); // 컨테이너의 ID 확인
-            json.put("Process Id"+i,data.get("Id"));
-            Thread.sleep(100);
+            object.put("Process ID",data.get("Id"));
 
             String obj = data.get("Names").toString();
             obj = obj.substring(1,obj.length()-1); // 각 컨테이너의 이름으로 docker engine에서 stats를 호출
             System.out.println("container name : " + obj.substring(1,obj.length())); // 컨테이너의 이름 호출
-            json.put("container name"+i,obj.substring(1,obj.length()));
-            Thread.sleep(100);
+            object.put("container name",obj.substring(1,obj.length()));
 
             // 수행중인 컨테이너의 stats 정보 호출
             Process process = Runtime.getRuntime().exec("curl -s --unix-socket /var/run/docker.sock http://v1.41/containers"+obj+"/stats");
@@ -136,14 +138,13 @@ public class Monitoring {
 
             Long cpu_num = Long.parseLong(cpu_stats.get("online_cpus").toString()); // 컨테이너마다 할당된 cpu 개수 확인
             System.out.println("Cpu num : "+ cpu_stats.get("online_cpus"));
-            json.put("CPU num"+i,cpu_stats.get("online_cpus"));
-            Thread.sleep(100);
+            object.put("CPU num",cpu_stats.get("online_cpus"));
 
             Map<String,Object> memory_stats = (Map<String,Object>)stats.get("memory_stats");
             Long Climit = Long.parseLong(memory_stats.get("limit").toString()) / 8000000 ; // 호출 값이 bit -> byte 변환
             System.out.println("할당된 메모리 크기 : "+ Climit +"MB"); // 컨테이너마다 할당된 메모리 크기
-            json.put("container memory"+i,Climit);
-            Thread.sleep(100);
+            object.put("container memory",Climit);
+
             // CPU 사용률
             // cpu_delta = cpu_stats.cpu_usage.total_usage - precpu_stats.cpu_usage.total_usage
             // system_cpu_delta = cpu_stats.system_cpu_usage - precpu_stats.system_cpu_usage
@@ -160,8 +161,7 @@ public class Monitoring {
             String percent = String.format("%.5f",cpu_percent);
             CpuUse = cpu_percent;
             System.out.println("CPU 사용률 : "+ percent+"%");
-            json.put("CPU usage"+i,percent);
-            Thread.sleep(100);
+            object.put("CPU percent",percent);
 
             // 메모리 점유율, 사용중인 메모리
             // available_memory = memory_stats.limit
@@ -178,8 +178,7 @@ public class Monitoring {
 
             Long used_memory = usage ; // - cache;
             System.out.println("사용중인 메모리 : "+used_memory);
-            json.put("used memory"+i,used_memory);
-            Thread.sleep(100);
+            object.put("memory usage",used_memory);
 
             totalUseMemory +=(used_memory / 8000000);
 
@@ -188,34 +187,24 @@ public class Monitoring {
             Double memory_percent = (double)(used_memory) / memory_limit * 100.0;
             String m_percent = String.format("%.3f",memory_percent);
             System.out.println("Memory 점유률 : "+m_percent+"%");
-            json.put("memory usage"+i,m_percent);
-            Thread.sleep(100);
+            object.put("memory percent",m_percent);
 
             //네트워크 : tx , tx 패킷 개수 , rx , rx 패킷 개수
             // 축적값이 아니기 때문에 전값 유지
             Map<String,Object> networks = (Map<String, Object>) stats.get("networks");
-
-            /*
-            * networks 를 key로 하는 json 정보에 대해 ...
-            * 명칭이 docker engine 사이트에서느 eth0 , eth5 이던데 원인을 찾지 못했습니다.
-            * 제 컴퓨터의 도커 컨테이너들은 전부 eth0 , eth1 입니다
-            * 그래서 아래와 같이 코드를 구성하였기 때문에 확인시 실행이 되지 않는다면 eth5로 변경하여 실행 부탁드립니다.
-            */
-
             Map<String,Object> eth0 = (Map<String, Object>) networks.get("eth0");
-            Map<String,Object> eth1 = (Map<String, Object>) networks.get("eth1");
 
-            int rxb0 = Integer.parseInt(eth0.get("rx_bytes").toString()) + Integer.parseInt(eth1.get("rx_bytes").toString());
-            int rxp0 = Integer.parseInt(eth0.get("rx_packets").toString()) + Integer.parseInt(eth1.get("rx_packets").toString());
-            int txb0 = Integer.parseInt(eth0.get("tx_bytes").toString()) + Integer.parseInt(eth1.get("tx_bytes").toString());
-            int txp0 = Integer.parseInt(eth0.get("tx_packets").toString()) + Integer.parseInt(eth1.get("tx_packets").toString());
+            int rxb0 = Integer.parseInt(eth0.get("rx_bytes").toString());
+            int rxp0 = Integer.parseInt(eth0.get("rx_packets").toString());
+            int txb0 = Integer.parseInt(eth0.get("tx_bytes").toString());
+            int txp0 = Integer.parseInt(eth0.get("tx_packets").toString());
 
             if(packet_arr[i]==null)
             {
                 System.out.println("-----모니터링 후 첫 출력이므로 중간에 실행 시 누적값 출력-----\n ");
                 packet_arr[i]= new RxTx(rxb0,rxp0,txb0,txp0);
                 System.out.println(packet_arr[i].toString());
-                json.put("network"+i,packet_arr[i].toString());
+                object.put("network",packet_arr[i].toString());
             }
             else
             {
@@ -227,10 +216,9 @@ public class Monitoring {
                 packet_arr[i] = new RxTx(rxb0,rxp0,txb0,txp0);
                 String packet_result = new RxTx(rxb1,rxp1,txb1,txp1).toString();
                 System.out.println(packet_result);
-                json.put("network"+i,packet_arr[i].toString());
+                object.put("network",packet_result);
             }
-            Thread.sleep(100);
-
+            json.put(i,object.toJSONString()) ;
             i++;
         }
         return CpuUse;
